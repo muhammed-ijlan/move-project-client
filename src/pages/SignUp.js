30562a93c00741a1ab3f3c00c96e3680d817aef4
Muhammed Ijlan <ijlanijlu580@gmail.com>
2022-10-07 17:43:30 +0530

    signup page added

--- src/pages/SignUp.js
+++ src/pages/SignUp.js
@@ -1,8 +1,23 @@
+import { Grid, Paper, Stack, TextField, Typography, Button } from '@mui/material'
 import React from 'react'
+import { Link } from "react-router-dom"
 
 function SignUp() {
     return (
-        <div>SignUp</div>
+        <Grid container>
+            <Paper elevation={10} sx={{ padding: "20px", height: "55vh", width: "300px", margin: "auto", borderRadius: "10px" }}>
+                <Stack display='flex' alignItems="center">
+                    <Typography variant='h5'>SignUp</Typography>
+                </Stack>
+                <Stack spacing={4} sx={{ mt: "20px" }} color="white">
+                    <TextField fullWidth color='info' type="text" label="Full Name" id="fullWidth" />
+                    <TextField fullWidth color='info' type="email" label="Email" id="fullWidth" />
+                    <TextField fullWidth color='info' type="password" label="Password" id="fullWidth" />
+                    <Button variant='contained' color='success' fullWidth>SignIn</Button>
+                </Stack>
+                <Typography variant='body2' marginTop={1}>Already Have an Account? <Link to="/signin">SignIn</Link></Typography>
+            </Paper>
+        </Grid>
     )
 }
 
