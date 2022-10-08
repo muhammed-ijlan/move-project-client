6eb88c603f51a146c5eb675e21405887261a03d1
Muhammed Ijlan <ijlanijlu580@gmail.com>
2022-10-08 12:06:46 +0530

    signin page implemented

--- src/pages/Signin.js
+++ src/pages/Signin.js
@@ -2,22 +2,31 @@ import { Button, Grid, Paper, Stack, TextField, Typography } from '@mui/material
 import axios from 'axios'
 import React, { useState } from 'react'
 import { Link } from "react-router-dom"
+import { useDispatch, useSelector } from "react-redux"
+import { loginFailure, loginStart, loginSuccess } from '../redux/userSlice'
+
+
 
 
 function Signin() {
-    g
 
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
 
+    const dispatch = useDispatch();
+
+    const { isLoading, currentUser } = useSelector((state) => state.user)
+
     const signUser = async (e) => {
         e.preventDefault();
         try {
+            dispatch(loginStart())
             const res = await axios.post("http://localhost:4000/auth/signin", { email, password })
-            console.log(res.data);
+            dispatch(loginSuccess(res.data.user))
 
         } catch (e) {
             console.log(e);
+            dispatch(loginFailure())
         }
     }
 
@@ -30,9 +39,11 @@ function Signin() {
                 <form onSubmit={signUser}>
                     <Stack spacing={4} sx={{ mt: "20px" }} color="white">
 
-                        <TextField onChange={(e) => setEmail(e.target.value)} fullWidth color='info' name='email' type="email" label="Email" id="fullWidth" />
-                        <TextField onChange={(e) => setPassword(e.target.value)} fullWidth color='info' name='password' type="password" label="Password" id="fullWidth" />
-                        <Button type='submit' variant='contained' color='success' fullWidth>SignIn</Button>
+                        <TextField required onChange={(e) => setEmail(e.target.value)} fullWidth color='info' name='email' type="email" label="Email" id="fullWidth" />
+                        <TextField required helperText="" onChange={(e) => setPassword(e.target.value)} fullWidth color='info' name='password' type="password" label="Password" id="fullWidth" />
+                        {isLoading && <Button type='submit' variant='contained' color='success' fullWidth>Loading...</Button>}
+                        {!isLoading && <Button type='submit' variant='contained' color='success' fullWidth>LogiIn</Button>}
+
                     </Stack>
                 </form>
                 <Typography variant='body2' marginTop={1}>Dont Have an Account? <Link to="/signup">SignUp</Link></Typography>
--- src/redux/userSlice.js
+++ src/redux/userSlice.js
@@ -2,24 +2,23 @@ import { createSlice } from "@reduxjs/toolkit";
 
 const initialState = {
     currentUser: null,
-    loading: false
+    isLoading: false
 }
 
 const userSlice = createSlice({
     name: "user",
-
     initialState,
     reducers: {
         loginStart: (state) => {
-            state.loading = true;
+            state.isLoading = true;
         },
         loginSuccess: (state, action) => {
-            state.loading = false;
+            state.isLoading = false;
             state.currentUser = action.payload;
         },
         loginFailure: (state) => {
             state.currentUser = null;
-            state.loading = false;
+            state.isLoading = false;
         }
     }
 
